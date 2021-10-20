6f25ddc326c41f5b4634389b8ba025ba1b51d88b
Caleb Oke <seyioke30@gmail.com>
2021-10-20 14:46:45 -0400

    add service_test file

---
+++ MovieDatabases/services/service_test.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"MovieDatabases/entities"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreate(t *testing.T) {
+	jsonData1 := entities.Movie{
+		Id:          "",
+		Title:       "",
+		Genre:       []string{"suspense"},
+		Description: "People are forced to play a game to win money",
+		Director:    "Don't know",
+		Actors:      []string{"Sang Woo"},
+		Rating:      "78%",
+	}
+
+	s := MovieService{}
+
+	err := s.Create(jsonData1)
+	assert.EqualErrorf(t, err, BadRequest.Error(), "Error should be %v, but got %v", BadRequest.Error(), err)
+
+	//Need to test for the setId also
+
+}
+
+func TestReadById(t *testing.T) {
+	id := ""
+
+	s := MovieService{}
+	_, err := s.ReadbyId(id)
+
+	assert.EqualErrorf(t, err, BadRequest.Error(), "Error should return %v, but got %v", BadRequest.Error(), err)
+
+}
